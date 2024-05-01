f87a6601105231fcf989a739d81a753fdb43ff8d
minakshirajput12 <158456976+minakshirajput12@users.noreply.github.com>
2024-04-30 21:56:27 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+        function login() {
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
+            console.log("Username or Email: " + username);
+            console.log("Password: " + password);
+        }
